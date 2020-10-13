1947e769bcfec3f05fb88953d7c134be3a183873
Naveen DA <mailtomassnaveen@gmail.com>
2020-10-13 22:05:38 +0530

    Fix the js file not loading issue

--- src/next.config.js
+++ src/next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://raw.githubusercontent.com/NaveenDA/naveenda.github.io/master/docs/' : '',
+  assetPrefix: isProd ? 'https://raw.githubusercontent.com/NaveenDA/naveenda.github.io/gh-pages/' : '',
 }
