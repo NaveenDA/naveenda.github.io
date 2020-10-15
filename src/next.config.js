1d28a57d3d2af8fb9a5a2c75812c41f03c84ea57
Naveen DA <mailtomassnaveen@gmail.com>
2020-10-16 00:21:57 +0530

    Change the CDN

--- src/next.config.js
+++ src/next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://raw.githubusercontent.com/NaveenDA/naveenda.github.io/gh-pages/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
 }
