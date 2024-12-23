8e846efc86fdcbe543da1147b91302c0d9228b0d
NaveenDA <mailtomassnaveen@gmail.com>
2024-12-23 04:29:28 +0000

    Change from template

--- _next/static/chunks/app/blog/[slug]/page-97c061c4062c835a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[308],{9414:function(){}},function(n){n.O(0,[192,221,744],function(){return n(n.s=9414)}),_N_E=n.O()}]);
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // generate html output in the /dist folder
+  output: "export",
+};
+
+export default nextConfig;
